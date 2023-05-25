d2e5e089b77430ddbcb38df4e6d81321fae504f7
Ram Pujan Singh <rpujan@insightresults.com>
2023-05-25 10:44:37 +0530

    minor changes

--- 1-basic-fetch.js
+++ 1-basic-fetch.js
@@ -3,9 +3,30 @@ export function getJsonData(url) {
     .then((resp) => {
         return resp.json()
         .then((data) => {
-            console.log(data)            
+            //groupByContinents(data);
+            findByName(data, 'India')
+            // console.log(data)            
         }).catch((err) => {
             console.log(err)
         })
     });
 }
+
+// groyp by continenets
+function groupByContinents(data) {
+    let result = _.groupBy(data, 'continents');
+    console.log(result);
+}
+
+// get unique languages
+
+
+// get sorted by name
+
+// find any country by name
+
+function findByName(collection, name) {
+    let result = _.find(collection, function(o) { return o.name.common === name});
+    console.log(result);
+}
+// 
--- main.js
+++ main.js
@@ -2,5 +2,6 @@ import { getJsonData } from './1-basic-fetch.js';
 
 
 const url = 'https://restcountries.com/v3.1/all';
+const url2 = 'https://api.publicapis.org/entries';
 
 getJsonData(url);
