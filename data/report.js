5872314b4a79ba95b4fca5674149b3468e3b5c19
Ram Pujan Singh <rpujan@insightresults.com>
2023-06-02 15:09:22 +0530

    Data added

---
+++ data/report.js
@@ -0,0 +1,516 @@
+const reportData = [
+    {
+        "subtopic": "Ambience",
+        "avg": 4.33,
+        "count": 6,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 4,
+        "r5": 2
+    },
+    {
+        "subtopic": "Atmosphere",
+        "avg": 4,
+        "count": 12,
+        "r1": 0,
+        "r2": 3,
+        "r3": 0,
+        "r4": 3,
+        "r5": 6
+    },
+    {
+        "subtopic": "Attentive Service",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Attentiveness",
+        "avg": 5,
+        "count": 9,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 9
+    },
+    {
+        "subtopic": "Communication",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Creativity",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Custom Drinks",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Customization",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Denim And Diamonds Party",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Event",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Experience",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Expertise",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Food",
+        "avg": 4.67,
+        "count": 12,
+        "r1": 0,
+        "r2": 0,
+        "r3": 2,
+        "r4": 0,
+        "r5": 10
+    },
+    {
+        "subtopic": "Freshness",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Friendliness",
+        "avg": 5,
+        "count": 3,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 3
+    },
+    {
+        "subtopic": "Fun",
+        "avg": 5,
+        "count": 15,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 15
+    },
+    {
+        "subtopic": "Great Service",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Guacamole Making",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Host",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Knowledge",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Knowledgeable",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Management",
+        "avg": 2,
+        "count": 1,
+        "r1": 0,
+        "r2": 1,
+        "r3": 0,
+        "r4": 0,
+        "r5": 0
+    },
+    {
+        "subtopic": "Margarita Class",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Memorable",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Memorable Experience",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Menu",
+        "avg": 4,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 1,
+        "r5": 0
+    },
+    {
+        "subtopic": "Mushrooms Enchiladas",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Outdoor Seating",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Overall Experience",
+        "avg": 4,
+        "count": 3,
+        "r1": 0,
+        "r2": 1,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Personable",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Personality",
+        "avg": 5,
+        "count": 21,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 21
+    },
+    {
+        "subtopic": "Positive",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Preparation",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Presentation",
+        "avg": 5,
+        "count": 6,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 6
+    },
+    {
+        "subtopic": "Pricing",
+        "avg": 4,
+        "count": 3,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 3,
+        "r5": 0
+    },
+    {
+        "subtopic": "Professionalism",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Provider",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Recommend",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Recommendation",
+        "avg": 5,
+        "count": 3,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 3
+    },
+    {
+        "subtopic": "Reservation",
+        "avg": 2.5,
+        "count": 2,
+        "r1": 1,
+        "r2": 0,
+        "r3": 0,
+        "r4": 1,
+        "r5": 0
+    },
+    {
+        "subtopic": "Respectful Service",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Server",
+        "avg": 5,
+        "count": 3,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 3
+    },
+    {
+        "subtopic": "Service",
+        "avg": 4.86,
+        "count": 176,
+        "r1": 0,
+        "r2": 0,
+        "r3": 8,
+        "r4": 8,
+        "r5": 160
+    },
+    {
+        "subtopic": "Specialist",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Specialty Drink",
+        "avg": 5,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 2
+    },
+    {
+        "subtopic": "Taste",
+        "avg": 3,
+        "count": 2,
+        "r1": 0,
+        "r2": 0,
+        "r3": 2,
+        "r4": 0,
+        "r5": 0
+    },
+    {
+        "subtopic": "Venue",
+        "avg": 4.67,
+        "count": 3,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 1,
+        "r5": 2
+    },
+    {
+        "subtopic": "Wait Time",
+        "avg": 1,
+        "count": 1,
+        "r1": 1,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 0
+    },
+    {
+        "subtopic": "Waiter",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Waiter Fantastic",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    },
+    {
+        "subtopic": "Waitstaff",
+        "avg": 5,
+        "count": 1,
+        "r1": 0,
+        "r2": 0,
+        "r3": 0,
+        "r4": 0,
+        "r5": 1
+    }
+];
+
+export function getReportData() {
+  return reportData;
+}
